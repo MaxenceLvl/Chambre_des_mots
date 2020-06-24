408e6a162cde915a64c9a00e6cc0d9fba152a6fa
Maxence Levelu <maxence.levelu@outlook.fr>
2020-06-24 20:04:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/roomwordssample/WordRepository.kt
@@ -0,0 +1,15 @@
+package com.example.android.roomwordssample
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
